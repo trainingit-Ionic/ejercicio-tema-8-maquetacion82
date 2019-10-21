8bbba9c99efe392730b34481b913d11398913651
maquetacion82 <eugenia.gil@altia.es>
2019-10-21 17:47:31 +0200

    linterna

---
+++ linterna-maquetacion82/src/app/home/home.page.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { Flashlight } from '@ionic-native/flashlight/ngx';
+import { Platform, ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  flash: boolean;
+  constructor(private flashlight: Flashlight, public platform: Platform, public toastController: ToastController) {
+    this.platform.platforms().filter((item) => {
+      if ((item === 'mobile') || (item === 'tablet')) {
+        if (this.flashlight.available()) {
+          this.flash = true;
+        } else {
+          this.flash = false;
+          this.showToast();
+        }
+      } else {
+        this.flash = false;
+        this.showToast();
+      }
+    });
+
+    this.platform.pause.subscribe(
+      () => this.flashlight.switchOff()
+    );
+
+    /* this.platform.backButton.subscribe(
+      () => this.flashlight.switchOff()
+    ); */
+  }
+
+  async showToast() {
+    const toast = await this.toastController.create({
+      message: 'El dispositivo no dispone de flash',
+      showCloseButton: true,
+      closeButtonText: 'Ok',
+      position: 'top',
+    });
+    toast.present();
+  }
+}
