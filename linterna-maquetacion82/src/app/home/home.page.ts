516abebaf72ec1c4c2d7b24bc6d3430b4b0350df
maquetacion82 <eugenia.gil@altia.es>
2019-10-22 15:33:39 +0200

    cambios

--- linterna-maquetacion82/src/app/home/home.page.ts
+++ linterna-maquetacion82/src/app/home/home.page.ts
@@ -11,27 +11,35 @@ export class HomePage {
 
   flash: boolean;
   constructor(private flashlight: Flashlight, public platform: Platform, public toastController: ToastController) {
-    this.platform.platforms().filter((item) => {
-      if ((item === 'mobile') || (item === 'tablet')) {
-        if (this.flashlight.available()) {
-          this.flash = true;
+    this.platform.ready().then(() => {
+      this.platform.platforms().filter((item) => {
+        if ((item === 'mobile') || (item === 'tablet')) {
+          if (this.flashlight.available()) {
+            this.flash = true;
+          } else {
+            this.flash = false;
+            this.showToast();
+          }
         } else {
           this.flash = false;
           this.showToast();
         }
-      } else {
-        this.flash = false;
-        this.showToast();
-      }
+      });
+      this.platform.pause.subscribe(
+        () => this.flashlight.switchOff()
+      );
+      this.platform.backButton.subscribe(
+        () => this.flashlight.switchOff()
+      );
     });
+  }
 
-    this.platform.pause.subscribe(
-      () => this.flashlight.switchOff()
-    );
-
-    /* this.platform.backButton.subscribe(
-      () => this.flashlight.switchOff()
-    ); */
+  flashButton() {
+    if (this.flashlight.isSwitchedOn()) {
+      this.flashlight.switchOff();
+    } else {
+      this.flashlight.switchOn();
+    }
   }
 
   async showToast() {
